f514d470b01e7da0bc409fc7bf4fab09f3c01a44
ahubbard <ahubbard@yext.com>
2021-03-23 17:43:11 +0000

    add null check on CDC

--- cards/cdccovidcard/component.js
+++ cards/cdccovidcard/component.js
@@ -22,7 +22,7 @@ class cdccovidcardCardComponent extends BaseCard['cdccovidcard'] {
       title: profile.name, // The header text of the card
       // subtitle: '', // The sub-header text of the card
       details: profile.answer ? ANSWERS.formatRichText(profile.answer, 'VIEW_WEBSITE','_top') : null, // The text in the body of the card
-      footnote: `Last updated ${profile.c_contentLastUpdatedDate} from <a href="https://www.cdc.gov/" target=”_blank”>The Centers for Disease Control and Prevention</a>`,
+      footnote: profile.c_contentLastUpdatedDate ? `Last updated ${profile.c_contentLastUpdatedDate} from <a href="https://www.cdc.gov/" target=”_blank”>The Centers for Disease Control and Prevention</a>`: null,
       // If the card's details are longer than a certain character count, you can truncate the
       // text. A toggle will be supplied that can show or hide the truncated text.
       // showMoreDetails: {
